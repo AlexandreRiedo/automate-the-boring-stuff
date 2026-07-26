6b12f51d58bc9b14b4823d381c0a8ad70fa37ca3
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-07-26 13:52:40 +0200

    chapter 10: Mad Libs

---
+++ 10-reading-and-writing-files/madLibs.py
@@ -0,0 +1,16 @@
+import re
+
+SUB = ("ADJECTIVE", "ADVERB", "NOUN", "VERB")
+
+with open("madLibs.in") as fin:
+    text = fin.read()
+
+with open("madLibs.out", "w") as fout:
+    out_text = text
+    for m in re.finditer(f"({'|'.join([x for x in SUB])})", text):
+        usr_input = input(
+            f"Enter a{' ' + m.group().lower() if m.group().lower()[0] != 'a' else 'n ' + m.group().lower()}:\n"
+        )
+        out_text = out_text.replace(m.group(), usr_input, 1)
+
+    fout.write(out_text)
