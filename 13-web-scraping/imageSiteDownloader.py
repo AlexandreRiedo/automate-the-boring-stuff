fe9f8ea17ba7acbc492804a114568dc4c4a13eef
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-07-31 12:10:12 +0200

    chapter 13: working img scraping!

--- 13-web-scraping/imageSiteDownloader.py
+++ 13-web-scraping/imageSiteDownloader.py
@@ -1,6 +1,6 @@
+import time
 from pathlib import Path
 
-import requests
 from rich import print as rprint
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,9 +12,19 @@ SEARCH_TERM = "Monet"
 
 # Setup
 rprint("")
-browser = webdriver.Firefox()
-browser.maximize_window()
+
 Path.mkdir(Path.cwd() / "artvee-images", exist_ok=True)
+
+download_dir = Path.cwd() / "artvee-images"
+options = webdriver.FirefoxOptions()
+options.set_preference(
+    "browser.download.folderList", 2
+)  # 2 = use the custom dir below.
+options.set_preference("browser.download.dir", str(download_dir))
+options.set_preference("browser.download.useDownloadDir", True)
+
+browser = webdriver.Firefox(options=options)
+browser.maximize_window()
 
 # Search
 browser.get("https://artvee.com/")
@@ -25,13 +35,18 @@ search.send_keys(SEARCH_TERM)
 search.send_keys(Keys.ENTER)
 
 # Download a page of images
-images = WDW(browser, 10).until(
-    EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'img[class="lazy"]'))
+time.sleep(10)
+img_divs = WDW(browser, 10).until(
+    EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div:has(> img.lazy)"))
 )
-for img_num, img in enumerate(images, 1):
-    img_src = img.get_attribute("src")
-    if img_src:
-        img_res = requests.get(img_src)
-        img_res.raise_for_status()
-        with open(f"./artvee-images/img{img_num:0>3}.png", "wb") as img_file:
-            img_file.write(img_res.content)
+img_page_urls = [
+    rf"https://artvee.com{img_div.get_attribute('data-url')}" for img_div in img_divs
+]
+for img_page_url in img_page_urls:
+    browser.get(img_page_url)
+    img_download = WDW(browser, 10).until(
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, ".snax-action-add-to-collection")
+        )
+    )
+    img_download.click()
