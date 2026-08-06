18836bb22ea82feb07c672b1c619980207c2d982
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-08-06 20:13:36 +0200

    chapter 14: Multiplication Table Maker

---
+++ 14-excel/multiplicationTable.py
@@ -0,0 +1,18 @@
+import sys
+
+import openpyxl
+from openpyxl.utils import get_column_letter as gcl
+
+wb = openpyxl.Workbook()
+sheet = wb.active
+assert sheet is not None
+
+sheet.title = "Multiplication Table"
+for offset in range(1, int(sys.argv[1]) + 1):
+    sheet[f"A{offset + 1}"] = offset
+    sheet[f"{gcl(offset + 1)}1"] = offset
+for col in range(1, int(sys.argv[1]) + 1):
+    for row in range(1, int(sys.argv[1]) + 1):
+        sheet.cell(row + 1, col + 1).value = row * col  # type: ignore
+
+wb.save("multiplicationTable.xlsx")
