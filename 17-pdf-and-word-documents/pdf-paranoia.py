a48f506783859aa6b189d7b8d0b5c185789fefc7
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-08-23 12:46:13 +0200

    chapter 17: PDF Paranoia Part 1

--- 17-pdf-and-word-documents/pdf-paranoia.py
+++ 17-pdf-and-word-documents/pdf-paranoia.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import pypdf
 from pypdf.errors import PdfReadError
 from rich import print as rprint
+from send2trash import send2trash
 
 password = " ".join(sys.argv[1:])
 
@@ -13,11 +14,20 @@ for root, dirs, files in os.walk(Path.cwd()):
         pdf_encrypted_filename = Path(root) / f"{pdf_filename.stem}_encrypted.pdf"
 
         try:
+            # Creating an encrypted copy
             writer = pypdf.PdfWriter(clone_from=pypdf.PdfReader(pdf_filename))
             writer.encrypt(password, algorithm="AES-256")
 
             with open(pdf_encrypted_filename, "wb") as file:
                 writer.write(file)
                 rprint(f"[green]Wrote: [blue]{pdf_encrypted_filename}\n")
+
+            # Making sure one can decrypt it
+            reader = pypdf.PdfReader(pdf_encrypted_filename)
+            if reader.is_encrypted:
+                reader.decrypt(password)
+
+            # Deleting the unencrypted file
+            send2trash(pdf_filename)
         except (OSError, ValueError, PdfReadError) as e:
             rprint(f"[red]Error [blue]{pdf_encrypted_filename} with [green]{e}\n")
