e7091b3fae4efd047bed8afd0b2a08e8f78afc50
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-23 23:54:27 +0200

    chapter 23: Game Playing Bot (issues with the image detection)

--- 23-controlling-the-keyboard-and-mouse/game-playing-bot.py
+++ 23-controlling-the-keyboard-and-mouse/game-playing-bot.py
@@ -1,4 +1,7 @@
+from collections import deque
+
 import pyautogui as pag
+from rich import print as rprint
 
 IMG_PATH = "screenshots/"
 
@@ -8,13 +11,25 @@ firefox.activate()
 firefox.maximize()
 pag.sleep(1)
 
-pag.keyDown("shiftleft")
-pag.click(f"{IMG_PATH}firefox-reload.png")
-pag.keyUp("shiftleft")
-pag.sleep(1.5)
+while True:
+    pag.keyDown("shiftleft")
+    pag.click(f"{IMG_PATH}firefox-reload.png")
+    pag.keyUp("shiftleft")
+    pag.sleep(1)
 
-pag.click(f"{IMG_PATH}play-game.png")
-pag.sleep(7.5)
+    pag.click(f"{IMG_PATH}play-game.png")
+    pag.sleep(2.5)
+
+    try:
+        pag.click(f"{IMG_PATH}play-error.png")
+    except pag.ImageNotFoundException:
+        rprint("[green]Game loaded fine")
+        break
+    else:
+        rprint("[red]Error loading, trying again")
+
+pag.sleep(5)
+
 
 pag.click(f"{IMG_PATH}sound.png")
 
@@ -26,3 +41,25 @@ pag.click(skip)
 pag.sleep(0.5)
 
 pag.click(f"{IMG_PATH}continue.png")
+pag.sleep(0.5)
+
+# Game loop
+customers = deque()
+foods = {
+    "california",
+    "gunkan",
+    "onigiri",
+}
+while True:
+    pag.sleep(1)
+
+    for food in foods:
+        try:
+            for customer in pag.locateAllOnScreen(f"{IMG_PATH}{food}.png"):
+                # if customer not in customers:
+                # customers.append(f"{food}: {customer}")
+                rprint(f"{food}: {customer=}")
+        except:
+            rprint(f"[blue]No {food} found.")
+
+    rprint(f"{customers=}")
