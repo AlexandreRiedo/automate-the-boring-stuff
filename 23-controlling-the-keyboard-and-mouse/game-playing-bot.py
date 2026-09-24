d65e3496860defa99af88fd1a49489737d7541e5
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-24 19:20:20 +0200

    chapter 23: Game Playing Bot (polling for food successful)

--- 23-controlling-the-keyboard-and-mouse/game-playing-bot.py
+++ 23-controlling-the-keyboard-and-mouse/game-playing-bot.py
@@ -1,9 +1,13 @@
 from collections import deque
+from typing import NamedTuple
 
 import pyautogui as pag
+import pyscreeze
 from rich import print as rprint
 
-IMG_PATH = "screenshots/"
+LAUNCH_PATH = "screenshots/launch"
+FOOD_PATH = "screenshots/food"
+CONF = 0.6
 
 # Launching the game
 firefox = pag.getWindowsWithTitle("Firefox")[0]  # pyright: ignore[reportAttributeAccessIssue]
@@ -11,55 +15,61 @@ firefox.activate()
 firefox.maximize()
 pag.sleep(1)
 
-while True:
-    pag.keyDown("shiftleft")
-    pag.click(f"{IMG_PATH}firefox-reload.png")
-    pag.keyUp("shiftleft")
-    pag.sleep(1)
+firefox_search = (893, 76)
+pag.click(firefox_search)
+pag.hotkey("ctrl", "a")
+pag.press("del")
+pag.write("https://armorgames.com/play/124/sushi-go-round")
+pag.press("enter")
+pag.sleep(1)
 
-    pag.click(f"{IMG_PATH}play-game.png")
-    pag.sleep(2.5)
+pag.click(f"{LAUNCH_PATH}/play-game.png")
+pag.sleep(7.5)
 
-    try:
-        pag.click(f"{IMG_PATH}play-error.png")
-    except pag.ImageNotFoundException:
-        rprint("[green]Game loaded fine")
-        break
-    else:
-        rprint("[red]Error loading, trying again")
+sound = pag.locateOnScreen(f"{LAUNCH_PATH}/sound.png", confidence=CONF)
+pag.click(sound)
 
-pag.sleep(5)
-
-
-pag.click(f"{IMG_PATH}sound.png")
-
-play = pag.locateOnScreen(f"{IMG_PATH}purple-play.png", minSearchTime=2)
+play = pag.locateOnScreen(f"{LAUNCH_PATH}/purple-play.png", confidence=CONF)
 pag.click(play)
 
-skip = pag.locateOnScreen(f"{IMG_PATH}yellow-skip.png", minSearchTime=2)
+skip = pag.locateOnScreen(f"{LAUNCH_PATH}/yellow-skip.png", confidence=CONF)
 pag.click(skip)
 pag.sleep(0.5)
 
-pag.click(f"{IMG_PATH}continue.png")
+start_game = pag.locateOnScreen(f"{LAUNCH_PATH}/start-game.png", confidence=CONF)
+pag.click(start_game)
 pag.sleep(0.5)
 
+
 # Game loop
-customers = deque()
+class Customer(NamedTuple):
+    food: str
+    box: pyscreeze.Box
+
+
+POLL = 1
+customers: deque[Customer] = deque()
 foods = {
     "california",
     "gunkan",
     "onigiri",
 }
-while True:
-    pag.sleep(1)
 
+while True:
+    pag.sleep(POLL)
     for food in foods:
         try:
-            for customer in pag.locateAllOnScreen(f"{IMG_PATH}{food}.png"):
-                # if customer not in customers:
-                # customers.append(f"{food}: {customer}")
-                rprint(f"{food}: {customer=}")
-        except:
+            for new_box in pag.locateAllOnScreen(
+                f"{FOOD_PATH}/{food}.png", confidence=CONF
+            ):
+                if not any(
+                    new_box.left - 25 < box.left < new_box.left + 25
+                    for box in [customer.box for customer in customers]
+                ):
+                    customers.append(Customer(food, new_box))
+        except (pag.ImageNotFoundException, pyscreeze.ImageNotFoundException):
             rprint(f"[blue]No {food} found.")
 
-    rprint(f"{customers=}")
+    for customer in customers:
+        rprint(f"{customer.food} {customer.box}")
+    rprint("")
