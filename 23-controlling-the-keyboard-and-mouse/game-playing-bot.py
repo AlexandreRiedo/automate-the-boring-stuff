1581b94ea9cce9d2b1f83ed7f520017fbdf3fd0d
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-24 20:14:30 +0200

    chapter 23: Game Playing Bot (adding customers, cooking food, removing customers)

--- 23-controlling-the-keyboard-and-mouse/game-playing-bot.py
+++ 23-controlling-the-keyboard-and-mouse/game-playing-bot.py
@@ -1,13 +1,28 @@
+import time
 from collections import deque
-from typing import NamedTuple
+from dataclasses import dataclass
 
 import pyautogui as pag
 import pyscreeze
 from rich import print as rprint
 
-LAUNCH_PATH = "screenshots/launch"
-FOOD_PATH = "screenshots/food"
-CONF = 0.6
+
+@dataclass
+class Customer:
+    box: pyscreeze.Box
+    food: str
+    handled_time: float | None
+    is_handled: bool = False
+
+
+CONF = 0.8
+CONF_FOOD = 0.6
+CONF_INGREDIENTS = 0.95
+PATH_FOOD = "screenshots/food"
+PATH_LAUNCH = "screenshots/launch"
+PATH_INGREDIENTS = "screenshots/ingredients"
+POLL = 1
+TIME_HANDLE = 22
 
 # Launching the game
 firefox = pag.getWindowsWithTitle("Firefox")[0]  # pyright: ignore[reportAttributeAccessIssue]
@@ -23,53 +38,94 @@ pag.write("https://armorgames.com/play/124/sushi-go-round")
 pag.press("enter")
 pag.sleep(1)
 
-pag.click(f"{LAUNCH_PATH}/play-game.png")
+pag.click(f"{PATH_LAUNCH}/play-game.png")
 pag.sleep(7.5)
 
-sound = pag.locateOnScreen(f"{LAUNCH_PATH}/sound.png", confidence=CONF)
+sound = pag.locateOnScreen(f"{PATH_LAUNCH}/sound.png", confidence=CONF)
 pag.click(sound)
 
-play = pag.locateOnScreen(f"{LAUNCH_PATH}/purple-play.png", confidence=CONF)
+play = pag.locateOnScreen(f"{PATH_LAUNCH}/purple-play.png", confidence=CONF)
 pag.click(play)
 
-skip = pag.locateOnScreen(f"{LAUNCH_PATH}/yellow-skip.png", confidence=CONF)
+skip = pag.locateOnScreen(f"{PATH_LAUNCH}/yellow-skip.png", confidence=CONF)
 pag.click(skip)
 pag.sleep(0.5)
 
-start_game = pag.locateOnScreen(f"{LAUNCH_PATH}/start-game.png", confidence=CONF)
+start_game = pag.locateOnScreen(f"{PATH_LAUNCH}/start-game.png", confidence=CONF)
 pag.click(start_game)
-pag.sleep(0.5)
+pag.sleep(1)
 
 
 # Game loop
-class Customer(NamedTuple):
-    food: str
-    box: pyscreeze.Box
-
-
-POLL = 1
 customers: deque[Customer] = deque()
 foods = {
     "california",
-    "gunkan",
     "onigiri",
+    "gunkan",
 }
+cook = pag.locateOnScreen(f"{PATH_INGREDIENTS}/cook.png", confidence=CONF_INGREDIENTS)
+nori = pag.locateOnScreen(f"{PATH_INGREDIENTS}/nori.png", confidence=CONF_INGREDIENTS)
+rice = pag.locateOnScreen(f"{PATH_INGREDIENTS}/rice.png", confidence=CONF_INGREDIENTS)
+roe = pag.locateOnScreen(f"{PATH_INGREDIENTS}/roe.png", confidence=CONF_INGREDIENTS)
+
 
 while True:
+    # Poll for any new customers
     pag.sleep(POLL)
     for food in foods:
         try:
             for new_box in pag.locateAllOnScreen(
-                f"{FOOD_PATH}/{food}.png", confidence=CONF
+                f"{PATH_FOOD}/{food}.png", confidence=CONF_FOOD
             ):
                 if not any(
-                    new_box.left - 25 < box.left < new_box.left + 25
-                    for box in [customer.box for customer in customers]
+                    new_box.left - 25 < customer.box.left < new_box.left + 25
+                    for customer in customers
                 ):
-                    customers.append(Customer(food, new_box))
+                    customers.append(Customer(new_box, food, None))
         except (pag.ImageNotFoundException, pyscreeze.ImageNotFoundException):
             rprint(f"[blue]No {food} found.")
 
-    for customer in customers:
-        rprint(f"{customer.food} {customer.box}")
-    rprint("")
+    # Deal with a customer
+    if any(not customer.is_handled for customer in customers):
+        customer = next(c for c in customers if not c.is_handled)
+        match customer.food:
+            case "california":
+                pag.click(rice)
+                pag.click(nori)
+                pag.click(roe)
+                pag.sleep(0.5)
+
+                pag.click(cook)
+                customer.is_handled = True
+                customer.handled_time = time.time()
+            case "onigiri":
+                pag.click(rice)
+                pag.click(rice)
+                pag.click(nori)
+                pag.sleep(0.5)
+
+                pag.click(cook)
+                customer.is_handled = True
+                customer.handled_time = time.time()
+            case "gunkan":
+                pag.click(rice)
+                pag.click(nori)
+                pag.click(roe)
+                pag.click(roe)
+                pag.sleep(0.5)
+
+                pag.click(cook)
+                customer.is_handled = True
+                customer.handled_time = time.time()
+
+    # Remove stale served customers
+    while (
+        customers
+        and customers[0].handled_time
+        and abs(time.time() - customers[0].handled_time) > TIME_HANDLE
+    ):
+        customers.popleft()
+
+    rprint()
+    for c in customers:
+        rprint(f"{c}")
